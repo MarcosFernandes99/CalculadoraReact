a65b584e4bb1adaa124abe44592a6a0ce051ec3e
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2023-01-04 23:28:26 -0300

    Projeto calculadora com react finalizado

---
+++ my-app/primeiroProjeto/src/Calculadora.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react"
+
+const Calculadora = () => {
+    const [num1, setNum1] = useState(0)
+    const [num2, setNum2] = useState(0)
+    const [result, setResult] = useState(0)
+    const [operation, setOperation] = useState("Somar")
+
+    useEffect(()=> {
+        setResult(calcular())
+    }, [num1, num2, operation])
+
+
+const calcular = () => {
+    if(operation === "Somar"){
+        return parseFloat(num1) + parseFloat(num2)
+    }
+    else if(operation === "Subtrair"){
+        return parseFloat(num1) - parseFloat(num2)
+    }
+    else if(operation === "Multiplicar"){
+        return parseFloat(num1) * parseFloat(num2)
+    }
+    else{
+        return parseFloat(num1)/parseFloat(num2)
+    }
+}
+
+    return (
+        <div className="calculadora">
+            <h1>Calculadora</h1>
+            <div>
+                <input type="number" placeholder="Numero um" value={num1} onChange={(e) => setNum1(e.target.value)} />
+            </div>
+            <div>
+                <input type="number" placeholder="Numero dois" value={num2} onChange={(e) => setNum2(e.target.value)} />
+            </div>
+            <select onChange={(e) => setOperation(e.target.value)}>
+                <option>Somar</option>
+                <option>Subtrair</option>
+                <option>Multiplicar</option>
+                <option>Dividir</option>
+            </select>
+
+            <p>Resultado = {result} </p>
+        </div>
+    )
+}
+
+export default Calculadora
