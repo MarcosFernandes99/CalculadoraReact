8d979b526f70fb0c27fd393fad161a77547e8a21
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2023-01-19 20:46:59 -0300

    inserindo CSS de informa inline styles

--- High-Order-Components/my-app/src/moleculas/Citation.tsx
+++ High-Order-Components/my-app/src/moleculas/Citation.tsx
@@ -7,14 +7,35 @@ export interface ICitation {
     nota?: number;
 }
 
+const quoteStyle = {
+    marginTop: '3vh',
+    margin: '2vh 2vh 2vh 2vh'
+}
+
+const quoteCardStyle = {
+    marginTop: '3vh',
+    border: '0.5vh solid black',
+    borderRadius: '3vh',
+    backgroundColor: 'white',
+    fontSize: '3vh',
+    
+}
+
+const animeAndCharacterStyle = {
+    marginTop: '2vh',
+    fontSize: '2vh',
+    margin: '2vh 2vh 2vh 2vh'
+}
+
+
 const Citation: React.FC<ICitation> = (props) => {
     return (
         <>
-            <div className="quoteCard">
-                <div className="quote">
+            <div style={quoteCardStyle} className="quoteCard">
+                <div style={quoteStyle} className="quote">
                     <Span>{props.quote}</Span>
                 </div>
-                <div>
+                <div style={animeAndCharacterStyle}>
                     {props.nota !== undefined ?
                         <>
                             <Span>{'Nota: ' + props.nota?.toString() || ''}</Span>
