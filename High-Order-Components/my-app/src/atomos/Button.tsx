5a27e307a61748ae962c6842e9ae2d899986c3c7
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2023-01-19 22:10:40 -0300

    adicionando styled-components em algum elemento

--- High-Order-Components/my-app/src/atomos/Button.tsx
+++ High-Order-Components/my-app/src/atomos/Button.tsx
@@ -1,4 +1,5 @@
-import {ICitation} from "../moleculas/Citation"
+import { ICitation } from "../moleculas/Citation"
+import styled from "styled-components";
 
 interface IButton {
     HandleVote: (quote?: ICitation, grade?: number) => void;
@@ -6,13 +7,26 @@ interface IButton {
     children: string;
 }
 
+const Btn = styled.button`
+    margin-top: 10vh;
+  text-align: center;
+  margin-left: 2vw;
+  width: 3vw;
+  height: 6vh;
+  border-radius: 20px;
+  cursor: pointer;
+  background-color: blue;
+  color: white
+`;
 
 const Button: React.FC<IButton> = (props) => {
-    
+
     return (
 
-        <button className="button" onClick={() => {props.HandleVote(props.quote,
-            parseInt(props.children))}}>{props.children}</button>
+        <Btn className="button" onClick={() => {
+            props.HandleVote(props.quote,
+                parseInt(props.children))
+        }}>{props.children}</Btn>
 
     )
 
